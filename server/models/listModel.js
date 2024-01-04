f322672def45c724cd15f9f5609c7266389879f9
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-04 18:20:59 -0500

    basic todoList model created

---
+++ server/models/listModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const todoListSchema = new mongoose.Schema({
+    todo: {
+        type: String,
+        required: "true"
+    }
+});
+
+const todoList = mongoose.model("todoList", todoListSchema);
+
+module.exports = todoList;
