a5f6fc6f7dfaf1fbbd5ea7209f28ddb76c47e6a2
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-03 19:44:13 -0500

    created user model

---
+++ server/models/userModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+    passwordVerify: {
+        type: String,
+        required: true
+    }
+});
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
