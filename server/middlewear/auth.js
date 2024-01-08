da6d95d0f11e2054d52081a18a32d75f7153d1c7
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-08 12:06:12 -0500

    validated JWT

--- server/middlewear/auth.js
+++ server/middlewear/auth.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+
 const auth = (req, res, next) => {
     try {
         const token = req.cookies.token;
@@ -5,6 +7,18 @@ const auth = (req, res, next) => {
         if (!token) {
             return res.status(401).json({ message: `Unauthorized.` })
         }
+
+        //checks if token is created with the JWT_SECRET;
+        const verifyJWT = jwt.verify(token, process.env.JWT_SECRET);
+        //if token has been created with the JWT_SECRET, decode the payload of the jwt
+
+        //console.log(verifyJWT)
+        req.user = verifyJWT.user;
+        console.log(req.user);
+
+        //middlewear function
+        //here, next will exit the auth middlewear, and will continue with the handle function in the listRouter.
+        next();
 
     } catch (err) {
         console.error(err);
