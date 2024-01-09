ad7852bf47d2364de4a90eeb3668674a317163b3
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-09 18:56:18 -0500

    navigate to home after logout

--- client/src/components/auth/Logout.jsx
+++ client/src/components/auth/Logout.jsx
@@ -1,16 +1,21 @@
 import axios from "axios";
 import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 
 const Logout = () => {
 
     const { getLoggedIn } = useContext(AuthContext);
+    const navigate = useNavigate();
 
     const logoutUser = async () => {
 
         //get logged out endpoint from server
         await axios.get("http://localhost:5000/auth/logout");
         getLoggedIn();
+
+        //redirect to homepage after logout
+        navigate("/");
     }
 
     return (
