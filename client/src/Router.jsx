66cc669c777d30dc244c5b7263fd0411cc8fbaed
erikajdavid <123127113+erikajdavid@users.noreply.github.com>
2024-01-09 12:00:58 -0500

    routing for Login component

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 const Router = () => {
   return (
@@ -10,7 +11,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<div>Home</div>} />
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<div>Login</div>} />
+        <Route path="/login" element={<Login />} /> 
         <Route path="/todolist" element={<div>To do list</div>} />
       </Routes>
     </BrowserRouter>
